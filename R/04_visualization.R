6e3b8b9d57c42bede7878f83817a4b85291d233e
Jordan-hollinger <jordan.a.hollinger@gmail.com>
2026-04-28 17:05:30 -0400

    updates to visualization.

--- R/04_visualization.R
+++ R/04_visualization.R
@@ -5,8 +5,12 @@ region_totals <- read_csv("data/processed/regional_walkshed_summary.csv", col_ty
 
 town_totals <- town_totals |>
   mutate(walk_time_min = factor(walk_time_min, levels = c(5, 10, 15)),
-         pct_within = pct_within / 100)
+         pct_within_fr = pct_within / 100)
 
+
+#---------
+#Food Retailers Walkshed by Town
+#---------
 
 town_order <- town_totals |>
   filter(destination == "Food Retailers") |>
@@ -20,17 +24,78 @@ town_totals |>
   filter(destination == "Food Retailers") |>
   ggplot(aes(x = town)) +
   geom_col(data = ~filter(.x, walk_time_min == 15), 
-           aes(y = pct_within), fill = "salmon",
+           aes(y = pct_within_fr, fill = "15 minutes"), 
            position = "identity") +
   geom_col(data = ~filter(.x, walk_time_min == 10), 
-           aes(y = pct_within), fill = "darkseagreen3", 
+           aes(y = pct_within_fr, fill = "10 minutes"), 
            position = "identity") +
   geom_col(data = ~filter(.x, walk_time_min == 5), 
-           aes(y = pct_within), fill = "steelblue",
+           aes(y = pct_within_fr, fill = "5 minutes"), 
            position = "identity") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  theme_minimal() +
+  scale_fill_manual(name = "Walk Time",
+                    values = c("5 minutes" = "steelblue", 
+                               "10 minutes" = "darkseagreen3", 
+                               "15 minutes" = "salmon"),
+                    breaks = c("15 minutes", "10 minutes", "5 minutes")) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 12),
+        axis.text.y = element_text(size = 12),
+        axis.title.y = element_text(size = 12),
+        axis.title.x = element_text(size = 12),
+        legend.position = c(0.99,0.99),
+        legend.justification = c("right", "top"),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 14),
+        plot.title = element_text(size = 14, face = "bold")) +
   labs(title = "Percentage of Population within Walkshed of Food Retailers by Town",
        x = "Town",
        y = "Percentage of Population") +
-  scale_y_continuous(labels = scales::percent)
+  scale_y_continuous(labels = scales::percent, limits = c(0, 1))
+
+ggsave("outputs/food_retailers_walkshed_by_town.png", width = 10, height = 6, dpi = 300)
+  
+
+#---------
+#Healthcare Walkshed by Town
+#---------
+
+ town_order <- town_totals |>
+  filter(destination == "Healthcare") |>
+  filter(as.numeric(as.character(walk_time_min)) == 15) |>
+  arrange(desc(pct_within)) |>
+  pull(town) |>
+  unique()
+
+town_totals |>
+  mutate(town = factor(town, levels = town_order)) |>
+  filter(destination == "Healthcare") |>
+  ggplot(aes(x = town)) +
+  geom_col(data = ~filter(.x, walk_time_min == 15), 
+           aes(y = pct_within_fr, fill = "15 minutes"), 
+           position = "identity") +
+  geom_col(data = ~filter(.x, walk_time_min == 10), 
+           aes(y = pct_within_fr, fill = "10 minutes"), 
+           position = "identity") +
+  geom_col(data = ~filter(.x, walk_time_min == 5), 
+           aes(y = pct_within_fr, fill = "5 minutes"), 
+           position = "identity") +
+  theme_minimal() +
+  scale_fill_manual(name = "Walk Time",
+                    values = c("5 minutes" = "steelblue", 
+                               "10 minutes" = "darkseagreen3", 
+                               "15 minutes" = "salmon"),
+                    breaks = c("15 minutes", "10 minutes", "5 minutes")) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 12),
+        axis.text.y = element_text(size = 12),
+        axis.title.y = element_text(size = 12),
+        axis.title.x = element_text(size = 12),
+        legend.position = c(0.99,0.99),
+        legend.justification = c("right", "top"),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 14),
+        plot.title = element_text(size = 14, face = "bold")) +
+  labs(title = "Percentage of Population within Walkshed of Healthcare Provider by Town",
+       x = "Town",
+       y = "Percentage of Population") +
+  scale_y_continuous(labels = scales::percent, limits = c(0, 1))
   
